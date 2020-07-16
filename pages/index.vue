12aef5027c5e3e14b640b6448aade1252ba36d87
Andrew <snookaw@gmail.com>
2020-07-16 09:07:36 +0100

    i have no idea...

--- pages/index.vue
+++ pages/index.vue
@@ -2,33 +2,26 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">
-        losthobbiesclient
-      </h1>
+      <h1 class="title">Lost Hobbies</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
           rel="noopener noreferrer"
           class="button--green"
-        >
-          Documentation
-        </a>
+        >Documentation</a>
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
-        >
-          GitHub
-        </a>
+        >GitHub</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
 </script>
 
 <style>
@@ -42,16 +35,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
